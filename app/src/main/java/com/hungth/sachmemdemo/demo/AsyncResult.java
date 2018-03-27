57f5f3210d7e1892ad0ae6907faf385d6dc88f25
ngoth <ngothanh1809@gmail.com>
2018-03-27 16:15:28 +0700

    get data in google sheet

---
+++ app/src/main/java/com/hungth/sachmemdemo/demo/AsyncResult.java
@@ -0,0 +1,11 @@
+package com.hungth.sachmemdemo.demo;
+
+import org.json.JSONObject;
+
+/**
+ * Created by kstanoev on 1/14/2015.
+ */
+interface AsyncResult
+{
+    void onResult(JSONObject object);
+}
---
+++ app/src/main/java/com/hungth/sachmemdemo/demo/DownloadWebpageTask.java
@@ -0,0 +1,94 @@
+package com.hungth.sachmemdemo.demo;
+
+import android.os.AsyncTask;
+import android.util.Log;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class DownloadWebpageTask extends AsyncTask<String, Void, String> {
+    AsyncResult callback;
+
+    public DownloadWebpageTask(AsyncResult callback) {
+        this.callback = callback;
+    }
+
+    @Override
+    protected String doInBackground(String... urls) {
+
+        // params xuất phát từ cuộc gọi execute (): params [0] là url.
+        try {
+            return downloadUrl(urls[0]);
+        } catch (IOException e) {
+            return "Không thể tải xuống trang yêu cầu.";
+        }
+    }
+
+    // onPostExecute displays the results of the AsyncTask.
+    @Override
+    protected void onPostExecute(String result) {
+        //        loại bỏ các phần không cần thiết từ các phản ứng và xây dựng một JSON
+        int start = result.indexOf("{", result.indexOf("{") + 1);
+        int end = result.lastIndexOf("}");
+        String jsonResponse = result.substring(start, end);
+        try {
+            JSONObject table = new JSONObject(jsonResponse);
+            callback.onResult(table);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private String downloadUrl(String urlString) throws IOException {
+        InputStream is = null;
+
+        try {
+            URL url = new URL(urlString);
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setReadTimeout(10000 /* milliseconds */);
+            conn.setConnectTimeout(15000 /* milliseconds */);
+            conn.setRequestMethod("GET");
+            conn.setDoInput(true);
+            // Starts the query
+            conn.connect();
+            int responseCode = conn.getResponseCode();
+            is = conn.getInputStream();
+
+            String contentAsString = convertStreamToString(is);
+            Log.d("ahaha", contentAsString);
+            return contentAsString;
+        } finally {
+            if (is != null) {
+                is.close();
+            }
+        }
+    }
+
+    private String convertStreamToString(InputStream is) {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(is));
+        StringBuilder sb = new StringBuilder();
+
+        String line = null;
+        try {
+            while ((line = reader.readLine()) != null) {
+                sb.append(line + "\n");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                is.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return sb.toString();
+    }
+}
